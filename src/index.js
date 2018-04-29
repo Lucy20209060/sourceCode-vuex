41d0eb58cc2d983cd3154882da0ecf0a96f6a2e1
luc <550502661@qq.com>
2018-04-30 00:08:30 +0800

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// 源码入口文件 提供store的各种module的构建安装
 import { Store, install } from './store'
 import { mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from './helpers'
 
@@ -11,3 +12,9 @@ export default {
   mapActions,
   createNamespacedHelpers
 }
+/* 
+从这里可以看出 我们可以使用vuex的方法与属性 如最基本的store
+供给 Vue.use()全局挂载的install
+展开内容的mapState,mapMutations,mapGetters,mapActions
+创建基于命名空间的组件绑定辅助函数 createNamespacedHelpers
+*/
